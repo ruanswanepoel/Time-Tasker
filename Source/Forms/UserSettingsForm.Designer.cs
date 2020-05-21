d91db9325c247b6825a5de81975548b57f7c9ca0
PandaPlaysAll <josh.piper@live.com.au>
2020-05-21 14:11:33 +1000

    Initial UI for UserAccounts

--- Source/Forms/UserSettingsForm.Designer.cs
+++ Source/Forms/UserSettingsForm.Designer.cs
@@ -23,20 +23,145 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.SuspendLayout();
-			// 
-			// UserSettingsForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(360, 650);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "UserSettingsForm";
-			this.Text = "UserSettingsForm";
-			this.ResumeLayout(false);
+            this.pnlTasks = new System.Windows.Forms.Panel();
+            this.btnMenu = new System.Windows.Forms.PictureBox();
+            this.lblList = new System.Windows.Forms.Label();
+            this.btnUserSettings = new System.Windows.Forms.PictureBox();
+            this.btnCalendar = new System.Windows.Forms.PictureBox();
+            this.btnTasks = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnUserSettings)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCalendar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTasks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTasks
+            // 
+            this.pnlTasks.AutoScroll = true;
+            this.pnlTasks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.pnlTasks.Location = new System.Drawing.Point(0, 49);
+            this.pnlTasks.Name = "pnlTasks";
+            this.pnlTasks.Size = new System.Drawing.Size(360, 558);
+            this.pnlTasks.TabIndex = 9;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(106)))), ((int)(((byte)(130)))));
+            this.btnMenu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMenu.Image = global::TimeTasker.Properties.Resources.ButtonHamMenu;
+            this.btnMenu.Location = new System.Drawing.Point(0, -1);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(50, 50);
+            this.btnMenu.TabIndex = 11;
+            this.btnMenu.TabStop = false;
+            // 
+            // lblList
+            // 
+            this.lblList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(106)))), ((int)(((byte)(130)))));
+            this.lblList.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblList.ForeColor = System.Drawing.Color.White;
+            this.lblList.Location = new System.Drawing.Point(48, -1);
+            this.lblList.Name = "lblList";
+            this.lblList.Size = new System.Drawing.Size(312, 50);
+            this.lblList.TabIndex = 10;
+            this.lblList.Text = "User Account";
+            this.lblList.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnUserSettings
+            // 
+            this.btnUserSettings.Location = new System.Drawing.Point(240, -1);
+            this.btnUserSettings.Name = "btnUserSettings";
+            this.btnUserSettings.Size = new System.Drawing.Size(120, 50);
+            this.btnUserSettings.TabIndex = 8;
+            this.btnUserSettings.TabStop = false;
+            // 
+            // btnCalendar
+            // 
+            this.btnCalendar.Location = new System.Drawing.Point(120, -1);
+            this.btnCalendar.Name = "btnCalendar";
+            this.btnCalendar.Size = new System.Drawing.Size(120, 50);
+            this.btnCalendar.TabIndex = 7;
+            this.btnCalendar.TabStop = false;
+            // 
+            // btnTasks
+            // 
+            this.btnTasks.Location = new System.Drawing.Point(0, -1);
+            this.btnTasks.Name = "btnTasks";
+            this.btnTasks.Size = new System.Drawing.Size(120, 50);
+            this.btnTasks.TabIndex = 6;
+            this.btnTasks.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(106)))), ((int)(((byte)(130)))));
+            this.pictureBox1.Location = new System.Drawing.Point(240, 605);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(106)))), ((int)(((byte)(130)))));
+            this.pictureBox2.Location = new System.Drawing.Point(120, 605);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(120, 50);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(106)))), ((int)(((byte)(130)))));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 605);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(120, 50);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // UserSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 650);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pnlTasks);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.lblList);
+            this.Controls.Add(this.btnUserSettings);
+            this.Controls.Add(this.btnCalendar);
+            this.Controls.Add(this.btnTasks);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UserSettingsForm";
+            this.Text = "UserSettingsForm";
+            ((System.ComponentModel.ISupportInitialize)(this.btnMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnUserSettings)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCalendar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnTasks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
-	}
+
+		private System.Windows.Forms.Panel pnlTasks;
+		private System.Windows.Forms.PictureBox btnMenu;
+		private System.Windows.Forms.Label lblList;
+		private System.Windows.Forms.PictureBox btnUserSettings;
+		private System.Windows.Forms.PictureBox btnCalendar;
+		private System.Windows.Forms.PictureBox btnTasks;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
 }
