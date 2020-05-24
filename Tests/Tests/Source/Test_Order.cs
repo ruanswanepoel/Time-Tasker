445abeb4e61a4f5e56b19edcf227fe6afd0adac4
PandaPlaysAll <josh.piper@live.com.au>
2020-05-24 14:47:09 +1000

    Settings.SaveData
    Settings.SaveData

--- Tests/Tests/Source/Test_Order.cs
+++ Tests/Tests/Source/Test_Order.cs
@@ -11,7 +11,7 @@ namespace Tests.Source
     [TestFixture]
     public class Test_Order
     {
-        TasksForm form;
+        TaskList form;
         Task a;
         Task b;
         Task c;
@@ -20,22 +20,22 @@ namespace Tests.Source
         [SetUp]
         public void SetUp()
         {
-            form = new TasksForm();
+            form = new TaskList("afwaf", new List<Task>());
             a = new Task("a", false, new DateTime(2020, 05, 01), new DateTime(2020, 06, 01), 5);
             b = new Task("b", false, new DateTime(2020, 05, 02), new DateTime(2020, 06, 02), 4);
             c = new Task("c", false, new DateTime(2020, 05, 03), new DateTime(2020, 06, 03), 3);
-            form.Tasklist.AddTask(a);
-            form.Tasklist.AddTask(b);
-            form.Tasklist.AddTask(c);
+            form.AddTask(a);
+            form.AddTask(b);
+            form.AddTask(c);
         }
 
         [Test]
         public void SortOrderAlphabetically()
         {
-            form.Tasklist.SortByAlphabetical(); 
-            form.Tasklist.Tasks[0].Equals(a); //Message = "a"
-            form.Tasklist.Tasks[1].Equals(b); //Message = "b"
-            form.Tasklist.Tasks[2].Equals(c); //Message = "c"
+            form.SortByAlphabetical(); 
+            form.Tasks[0].Equals(a); //Message = "a"
+            form.Tasks[1].Equals(b); //Message = "b"
+            form.Tasks[2].Equals(c); //Message = "c"
 
         }
 
